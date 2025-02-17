0000815af3d11f066f1b7243a60d4d49c3dff0d1
MD Monirujjaman <monirbinkhaled1@gmail.com>
2025-02-17 23:13:26 +0600

    added partially the add money event handler

--- js/home.js
+++ js/home.js
@@ -1,1 +1,16 @@
-console.log('home added');
+document.getElementById('btn-add-money').addEventListener('click',function (event){
+  event.preventDefault();
+  const mobileInputField = document.getElementById('mobile-input-field').value;
+  console.log(mobileInputField);
+  const amountInputField = document.getElementById('amount-input-field').value;
+  console.log(amountInputField);
+  const pinInputField = document.getElementById('pin-input-field').value;
+  console.log(pinInputField);
+
+  
+});
+// if(pin === '1234'){
+//   console.log('add taka');
+// }else{
+//   alert('Try agin!')
+// }
