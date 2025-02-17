e7c9c794ca085f4a58f400e42267f3a4b39b4f56
MD Monirujjaman <monirbinkhaled1@gmail.com>
2025-02-18 00:11:00 +0600

    add money done

--- js/home.js
+++ js/home.js
@@ -1,16 +1,32 @@
 document.getElementById('btn-add-money').addEventListener('click',function (event){
   event.preventDefault();
   const mobileInputField = document.getElementById('mobile-input-field').value;
-  console.log(mobileInputField);
+  // console.log(mobileInputField);
   const amountInputField = document.getElementById('amount-input-field').value;
-  console.log(amountInputField);
+  // console.log(amountInputField);
   const pinInputField = document.getElementById('pin-input-field').value;
-  console.log(pinInputField);
-
-  
+  // console.log(pinInputField);
+  if(pinInputField === '1234'){
+    const mainBalance = document.getElementById('main-balance').innerText;
+    // console.log(mainBalance);
+    const mainBalanceNumber = parseFloat(mainBalance);
+    const amountNumber = parseFloat(amountInputField);
+    const newBalance = mainBalanceNumber + amountNumber;
+    console.log( newBalance);
+    document.getElementById('main-balance').innerText = newBalance;
+    console.log('added taka in your account');
+  }else{
+  alert('Try agin!')
+  };
 });
-// if(pin === '1234'){
-//   console.log('add taka');
-// }else{
-//   alert('Try agin!')
-// }
+
+
+
+
+// mainBalance = "9,400"; 
+// parseFloat(mainBalance) // => 9 (ভুল ফলাফল!)
+// console.log(mainBalance);
+// mainBalance = "9400"; 
+// parseFloat(mainBalance) // => 9400 (ঠিক ফলাফল!)
+// console.log(mainBalance);
+
